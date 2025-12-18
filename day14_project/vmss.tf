89c76dd702ce254518f490cd6dba78550105f163
sreelekshmirae <sreelekshmirae@gmail.com>
2025-12-18 22:52:11 +0530

    completed the project - day 14

--- day14_project/vmss.tf
+++ day14_project/vmss.tf
@@ -1,20 +1,56 @@
 resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss_terraform" {
-    name = "vmss-terraform"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    sku_name = "Standard_DS1_v2"
-    instances = 3
-    platform_fault_domain_count = 1
-    zone = ["1"]
+  name                        = "vmss-terraform"
+  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg.location
+  sku_name                    = "Standard_DS1_v2"
+  instances                   = 3
+  platform_fault_domain_count = 1
+  zones                       = ["1"]
 
-    user_data_base64 = base64encode(file("user-data.sh"))  //this is the data that run once the VM is created 
-    os_profile {
-        linux_configuration {
-            disable_password_authentication = true
-            admin_username = "azureuser"
-            admin_ssh_key {
-                username = "azureuser"
-                public_key = file(".ssh/key.pub")
-        }
+  user_data_base64 = base64encode(file("user-data.sh")) //this is the data that run once the VM is created 
+  os_profile {
+    linux_configuration {
+      disable_password_authentication = true
+      admin_username                  = "azureuser"
+      admin_ssh_key {
+        username   = "azureuser"
+        public_key = file(".ssh/key.pub")
+      }
     }
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-LTS-gen2"
+    version   = "latest"
+  }
+  os_disk {
+    storage_account_type = "Premium_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name                          = "nic"
+    primary                       = true
+    enable_accelerated_networking = false
+
+    ip_configuration {
+      name                                   = "ipconfig"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet1.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bepool.id]
+    }
+  }
+
+  boot_diagnostics {
+    storage_account_uri = ""
+  }
+
+  # Ignore changes to the instances property, so that the VMSS is not recreated when the number of instances is changed
+  lifecycle {
+    ignore_changes = [
+      instances
+    ]
+  }
 }
--- day14_project/vnet.tf
+++ day14_project/vnet.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "test" {
   name                = "terrafrom-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet1" {
@@ -20,86 +20,136 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.0.0/20"]
 }
 
-resource azurerm_network_security_group "myNSG" {
-    name                = "myNSG"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    security_rule {
-        name                       = "allow-http"
-        priority                   = 100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "allow-https"
-        priority                   = 102
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+resource "azurerm_network_security_group" "myNSG" {
+  name                = "myNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-https"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "myNSG" {
-  subnet_id                 = azurerm_subnet.subnet.id
+  subnet_id                 = azurerm_subnet.subnet1.id
   network_security_group_id = azurerm_network_security_group.myNSG.id
 }
 
 #public ip for load balancer
-resource "azurerm_public_ip" "example" {
-    name                = "lb-publicIP"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method   = "Static"
-    sku                 = "Standard"
-    zone               = ["1", "2", "3"]
-    domain_name_label = "${azurerm_resource_group.rg.name}-${random_pet.lb_hostname.id}"
-} 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "lb-publicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1", "2", "3"]
+  domain_name_label   = "${azurerm_resource_group.rg.name}-${random_pet.lb_hostname.id}"
+}
 
 #loadbalancer
-resource "azurerm_lb" "example" {
-    name                = "myLB"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    sku                 = "Standard"
-    frontend_ip_configuration {
-        name                 = "myPublicIP"
-        public_ip_address_id = azurerm_public_ip.example.id
-    }
+resource "azurerm_lb" "loadbalancer" {
+  name                = "myLB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  frontend_ip_configuration {
+    name                 = "myPublicIP"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
 }
 
 #backend address pool
 resource "azurerm_lb_backend_address_pool" "bepool" {
-    name                = "myBackendAddressPool"
-    loadbalancer_id     = azurerm_lb.example.id
+  name            = "myBackendAddressPool"
+  loadbalancer_id = azurerm_lb.loadbalancer.id
+}
+
+#loadbalancer probe
+resource "azurerm_lb_probe" "lb_probe" {
+  name            = "http-probe"
+  loadbalancer_id = azurerm_lb.loadbalancer.id
+  protocol        = "Http"
+  port            = 80
+  request_path    = "/"
 }
 
 #loadbalancer rule 
-resource "azurerm_lb_rule" "example" {
-    name = "http"
-    loadbalancer_id            = azurerm_lb.example.id
-    protocol                   = "Tcp"
-    frontend_port              = 80
-    backend_port               = 80
-    frontend_ip_configuration_name = "myPublicIP"
-    backend_address_pool_id    = [azurerm_lb_backend_address_pool.bepool.id]
-    probe_id                   = azurerm_lb_probe.example.id
+resource "azurerm_lb_rule" "lb_rule" {
+  name                           = "http"
+  loadbalancer_id                = azurerm_lb.loadbalancer.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "myPublicIP"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bepool.id]
+  probe_id                       = azurerm_lb_probe.lb_probe.id
+}
+
+resource "azurerm_lb_nat_rule" "nat_rule_ssh" {
+  name                           = "ssh"
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.loadbalancer.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50001
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "myPublicIP"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bepool.id
 }
 
 resource "azurerm_public_ip" "natgwpip" {
-    name = "natgw-publicIP"
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method = "Static"
-    sku = "Standard"
-    zone = ["1"]
+  name                = "nat-gw-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
 }
 
+resource "azurerm_nat_gateway" "natgw" {
+  name                    = "nat-Gateway"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "natgw_pip_assoc" {
+  nat_gateway_id       = azurerm_nat_gateway.natgw.id
+  public_ip_address_id = azurerm_public_ip.natgwpip.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "subnet_natgw_assoc" {
+  subnet_id      = azurerm_subnet.subnet1.id
+  nat_gateway_id = azurerm_nat_gateway.natgw.id
+} 
