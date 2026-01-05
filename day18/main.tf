50e3de8ccb2053955feda207deeee622d8a7feea
sreelekshmirae <sreelekshmirae@gmail.com>
2026-01-05 23:23:40 +0530

    day 18 project added

---
+++ day18/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "day18-rg"
+  location = "canadacentral"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "day18storageaccount"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "example-app-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_function_app" "funapp" {
+  name                = "linuxfunctionappsa"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  service_plan_id            = azurerm_service_plan.sp.id
+
+  site_config {
+    application_stack {
+      node_version = 18
+    }
+  }
+}
